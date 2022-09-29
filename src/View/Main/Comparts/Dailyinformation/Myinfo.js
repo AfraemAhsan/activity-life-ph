0c6549414037cd84298c2337ac5862a2af11ab7d
afraemahsan <afraemahsan24@gmail.com>
2022-09-30 01:40:26 +0600

    next level update

--- src/View/Main/Comparts/Dailyinformation/Myinfo.js
+++ src/View/Main/Comparts/Dailyinformation/Myinfo.js
@@ -3,14 +3,14 @@ import React from "react";
 const Myinfo = () => {
   return (
     <div className="flex items-center justify-between bg-blue-50 rounded-[10px] mt-5 p-3">
-      <UserInfocard value="55kg" title="Weight" />
-      <UserInfocard value="5.7" title="Height" />
-      <UserInfocard value="22yrs" title="Age" />
+      <MyInfocard value="55kg" title="Weight" />
+      <MyInfocard value="5.7" title="Height" />
+      <MyInfocard value="22yrs" title="Age" />
     </div>
   );
 };
 
-const UserInfocard = ({ value, title }) => {
+const MyInfocard = ({ value, title }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span className="text-2xl font-semibold">{value}</span>
