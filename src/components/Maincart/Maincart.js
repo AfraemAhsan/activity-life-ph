d9a5ddc4de394069b55e3a042448fd04ceaf5211
afraemahsan <afraemahsan24@gmail.com>
2022-09-29 10:55:15 +0600

    next level update

--- src/components/Maincart/Maincart.js
+++ src/components/Maincart/Maincart.js
@@ -8,15 +8,16 @@ const Maincart = () => {
     <div>
       <div className="flex flex-row text-center pl-[150px] pt-[50px] pb-[50px]">
         <img
-          src="https://cdn.pixabay.com/photo/2016/12/28/08/15/hummingbird-1935665__340.png"
+          src="https://template.canva.com/EAEFH9WI4ac/1/0/400w-F4QBmAsbiR4.jpg"
           alt=""
           className="w-[100px] rounded-lg  "
         />
-        <h3 className="pt-[40px]">Activity of Live</h3>
+        <h3 className="pt-[40px] pl-[10px] text-2xl text-emerald-500 font-extrabold">
+          Activity of <span className="text-sky-700">Live</span>
+        </h3>
       </div>
 
       <div className="cart-container">
-        {" "}
         <div>
           <Leftbody></Leftbody>
         </div>
--- src/components/Rightbody/Rightbody.js
+++ src/components/Rightbody/Rightbody.js
@@ -3,19 +3,26 @@ import React from "react";
 const Rightbody = () => {
   return (
     <div>
-      <div className="profile">
-        <h2>Zahid</h2>
-        <span>Location : </span>
+      <div className="flex flex-row profile">
+        <img
+          src="https://p.kindpng.com/picc/s/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"
+          className="w-[70px] h-[70px]"
+          alt=""
+        />
+        <div>
+          <h2>Zahid</h2>
+          <span>Location : </span>
+        </div>
       </div>
       <div className="about-healty">
         <h3>
-          75 <span>weight</span>{" "}
+          75 <span>weight</span>
         </h3>
         <h3>
-          75 <span>weight</span>{" "}
+          75 <span>weight</span>
         </h3>
         <h3>
-          75 <span>weight</span>{" "}
+          75 <span>weight</span>
         </h3>
       </div>
       <div className="second">
